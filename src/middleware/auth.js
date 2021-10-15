b97f581083cfee888db9baf96aea1a5b68d32a1a
Heger Arias Santos <hegel.0218@gmail.com>
2021-10-14 22:30:22 -0400

    algo raro ;v

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -9,7 +9,7 @@ const verifyToken = (req, res, next) => {
 		return res.status(403).send("A token is required for authentication");
 	}
 	try {
-		const decoded = jwt.verify(token, config.TOKEN_KEY);
+		const decoded = jwt.verify(token, config.TOKEN_KEY || "Algo raro");
 	} catch (err) {
 		return res.status(401).send("Invalid Token");
 	}
