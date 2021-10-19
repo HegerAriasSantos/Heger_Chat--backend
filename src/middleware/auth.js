31b45c7859efffe83bd07b8dc1eb9594390813ed
Heger Arias Santos <hegel.0218@gmail.com>
2021-10-19 19:42:44 -0400

    Env variables working

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -7,7 +7,7 @@ const verifyToken = (req, res, next) => {
 		return res.status(403).send("A token is required for authentication");
 	}
 	try {
-		const decoded = jwt.verify(token, process.env.TOKEN_KEY || "Algo raro");
+		const decoded = jwt.verify(token, process.env.TOKEN_KEY);
 	} catch (err) {
 		return res.status(401).send("Invalid Token");
 	}
